d0d23e67faa364df273521ceb85f254d5cca0855
zeasin <liuhaoran2007@gmail.com>
2022-03-13 16:45:30 +0800

    I don't know

--- src/main/java/xyz/cbfh/zeasin/Main.java
+++ src/main/java/xyz/cbfh/zeasin/Main.java
@@ -3,8 +3,11 @@ package xyz.cbfh.zeasin;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
@@ -18,6 +21,7 @@ public class Main implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MODID = "zeasin";
+	public static BlockEntityType<AmethystContainerEntity> AMETHYST_CONTAINER_ENTITY;
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 	public static final Block AMETHYST_CONTAINER = new AmethystContainer(FabricBlockSettings.of(Material.AMETHYST));
 	public static final ItemGroup AMETHYST_TECH_GROUP = FabricItemGroupBuilder.build(new Identifier("amethyst", "tech"), () -> new ItemStack(Items.AMETHYST_SHARD));
@@ -28,6 +32,7 @@ public class Main implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		AMETHYST_CONTAINER_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "zeasin:amethyst_container_entity", FabricBlockEntityTypeBuilder.create(AmethystContainerEntity::new, AMETHYST_CONTAINER).build(null));
 		Registry.register(Registry.BLOCK, new Identifier(MODID, "amethyst_container"), AMETHYST_CONTAINER);//注册方块
 		Registry.register(Registry.ITEM, new Identifier(MODID, "amethyst_container"), new BlockItem(AMETHYST_CONTAINER, new Item.Settings().group(AMETHYST_TECH_GROUP)));//将方块注册到ItemGroup中
 		Registry.register(Registry.ITEM, new Identifier(MODID, "amethyst_sword"), new SwordItem(new AmethystMaterial(), 3, -2.5f, new Item.Settings().group(Main.AMETHYST_TECH_GROUP)));
