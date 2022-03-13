82b54cd04b5de6ea56d8f2aa27a52ec7456c1187
zeasin <liuhaoran2007@gmail.com>
2022-03-13 16:45:26 +0800

    I don't know

---
+++ src/main/java/xyz/cbfh/zeasin/AmethystContainerEntity.java
@@ -0,0 +1,35 @@
+package xyz.cbfh.zeasin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+
+public class AmethystContainerEntity extends BlockEntity implements ImplementedInventory {
+
+    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(2, ItemStack.EMPTY);
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return items;
+    }
+
+    public AmethystContainerEntity(BlockPos pos, BlockState state) {
+        super(Main.AMETHYST_CONTAINER_ENTITY, pos, state);
+    }
+
+}
